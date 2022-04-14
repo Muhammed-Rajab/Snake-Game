1e662d6fa56ba7109539de701fa284b380372258
Muhammed Rajab <muhammedrajab101@gmail.com>
2022-04-14 17:13:16 +0530

    Added basic functionality and understand the logic

--- scripts/main.js
+++ scripts/main.js
@@ -1,71 +1,168 @@
 'use strict';
 const log = console.log;
 
-function main() {
+const grid = document.querySelector("#grid");
+const scoreDisplay = document.querySelector(".scoreDisplay");
 
-    /* Returns live HTMLCollection of elements */
-    const gameGrid = document.getElementById("game-grid");
-    const grids = gameGrid.getElementsByTagName("div");
+/*
+    Our snake is just an array!! Can you believe that?
+*/
 
-    /* Query selectors */
-    const score = document.querySelector(".score");
-    const startBtn = document.querySelector(".start-game");
+let width = 10;
+let currentSnake = [2, 1, 0];
+let currentIndex = 0, appleIndex = 0;
 
-    /* Variables */
-    let score = 0;
-    let speed = 0.9;
-    const widthN = 10;
-    let interval = 0;
-    let direction = 1;
-    let appleIndex = 0;
-    let intervalTime = 0;
-    let currentIndex = 0;
-    const currentSnake = [2, 1, 0]; /* 2 -> Head, 1 -> Body, 0 -> Tail */
+let score = 0;
+let speed = 0.8;
+let direction = 1;
 
-    /* Key codes */
-    const UP_KEY = 38;
-    const DOWN_KEY = 40;
-    const LEFT_KEY = 37;
-    const RIGHT_KEY = 39;
+let interval = 0;
+let intervalTime = 0;
 
-
-    /* Functions start here */
-    function generateGameGrid (n) {
-        for(let i = 0; i < n**2; i++){
-            gameGrid.appendChild(document.createElement("div"));
-        };
-        grids[0].className = "snake";
-        grids[6].className = "apple";
-    };
-
-    function manageKeyPress (e) {
-        
-        grids[currentIndex].classList.remove("snake");
-        const keyCode = e.keyCode;
-
-        switch(keyCode){
-
-            case RIGHT_KEY:
-                direction = 1;
-                break;
-            
-            case UP_KEY:
-                direction = -widthN;
-                break;
-            
-            case LEFT_KEY:
-                direction = -1;
-                break;
-            
-            case DOWN_KEY:
-                direction = widthN;
-                break;
-        }
-
-    };
-
-    generateGameGrid(widthN);
-    document.addEventListener('keyup', manageKeyPress);
+function createBoard() {
+    for (let i = 0; i < 100; i++){
+        const newGrid = document.createElement('div');
+        newGrid.dataset.index = i;
+        newGrid.classList.add('grid-div');
+        grid.appendChild(newGrid);
+    }
 };
 
-document.addEventListener("DOMContentLoaded", main);
+function startGame() {
+    
+    const squares = grid.querySelectorAll('div');
+
+    // * Generates a random apple in the grid
+    randomApple(squares);
+
+    // * Intial setups
+    direction = 1;
+    intervalTime = 1000;
+    
+    currentIndex = 0;
+    currentSnake = [2, 1, 0];
+    
+    scoreDisplay.textContent = score;
+
+    // * Adding snake class to current snake
+    currentSnake.forEach(idx => squares[idx].classList.add('snake'));
+
+    // * Intervally moving the snake and taking necessary actions on move
+    interval = setInterval(moveOutcome, intervalTime);
+
+    // * Helper methods
+    function randomApple(squares) {
+        
+        do {
+            appleIndex = Math.floor(Math.random() * squares.length);
+        } while (squares[appleIndex].classList.contains("snake"));
+        
+        squares[appleIndex].classList.add("apple");
+    };
+
+    function control(e) {
+        log("key pressed", )
+        switch(e.keyCode) {
+            
+            case 39: // Right
+                direction = 1;
+                break;
+            case 38: // Up
+                direction = -width;
+                break;
+            case 37: // Left
+                direction = -1;
+                break;
+            case 40: // Down
+                direction = +width;
+                break;
+        }
+    }
+    
+    function eatApple (squares, tailIndex) {
+        /*
+            If tail of the snake has apple class,
+                Remove the apple class
+                Add snake class
+                Push tail
+                Generate random apple
+                Increment score
+                Update score
+                clearInterval and update speed;
+                Set new interval
+        */
+
+        if (squares[currentSnake[0]].classList.contains("apple")) {
+            squares[currentSnake[0]].classList.remove("apple");
+            squares[tailIndex].classList.add("snake");
+            currentSnake.push(tailIndex);
+            randomApple(squares);
+            scoreDisplay.textContent = ++score;
+            clearInterval(interval);
+            intervalTime *= speed;
+            interval = setInterval(moveOutcome, intervalTime);
+        }
+    }
+
+    function checkForHits(squares) {
+        
+        const  hitBottomWall = currentSnake[0] + width >= width*width && direction === width;
+
+        const hitRightWall = currentSnake[0] % width === width - 1 && direction === 1;
+
+        const hitLeftWall = currentSnake[0] % width === 0 && direction === -1;
+
+        const hitTopWall = currentSnake[0] - width <= 0 && direction === -width;
+
+        const hitBody = squares[currentSnake[0] + direction].classList.contains("snake");
+        // const hitBody = false;
+
+        if (hitBody || hitBottomWall || hitLeftWall || hitRightWall || hitTopWall) {
+            return true;
+        } else {
+            return false;
+        }
+    };
+
+    function moveSnake(squares) {
+        
+        let tailIndex = currentSnake.pop();
+        log(tailIndex);
+        squares[tailIndex].classList.remove("snake");
+        currentSnake.unshift(currentSnake[0] + direction);
+
+        // * Current movement is done
+
+        eatApple(squares, tailIndex);
+        squares[currentSnake[0]].classList.add('snake');
+    };
+
+    function moveOutcome(){
+        
+        // const squares = document.querySelectorAll(".grid div");
+        const squares = document.getElementsByClassName("grid-div");
+
+        /*
+            TODO: Check if got hit
+            
+            If hit something
+                Stop game
+            Else
+                Move snake
+        */
+
+        if (checkForHits(squares)) {
+            alert("You hit something");
+            return clearInterval(interval);
+        } else {
+            moveSnake(squares);
+        }
+    };
+
+    window.addEventListener("keyup", control);
+};
+
+document.addEventListener("DOMContentLoaded", function() {
+    createBoard();
+    startGame();
+});
